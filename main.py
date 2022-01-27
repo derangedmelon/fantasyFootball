682e5c8657de7d2ec00a97c713eefa6f056f2f43
derangedmelon <derangedmelon@gmail.com>
2022-01-27 23:14:42 +0000

    Second Commit

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import requests
 import pandas as pd
 import numpy as np
+import json
 
 url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
 
 r = requests.get(url)
+json = r.json()
+print(json.keys())
+data = pd.DataFrame(json['elements'])
 
-data = pd.read_json(r.json())
+print(data.head())
