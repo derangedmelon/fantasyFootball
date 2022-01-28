66138710cdc76ef4cd5a7b2f84e2ebeacb53c5be
derangedmelon <derangedmelon@gmail.com>
2022-01-28 00:34:03 +0000

    Third commit, selected specific columns

--- main.py
+++ main.py
@@ -9,5 +9,7 @@ r = requests.get(url)
 json = r.json()
 print(json.keys())
 data = pd.DataFrame(json['elements'])
-
+print(list(data))
 print(data.head())
+newData = data[['first_name', 'second_name', 'form', 'team', 'total_points']]
+print(newData.head())
